ab213cdcf2a49e7c1b9183b8fe031c8f331978b7
Surya-palanisamy <suryasitit@gmail.com>
2025-02-11 20:14:31 +0530

    fix:hire me button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import "../css/header.css"; // Import the CSS file
+import "../css/Header.css"; // Import the CSS file
 
 export default function Header() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
